1aafdc64ab4175d40b3edf3c1eccb6ceaf43aff3
Arthur Maurício <arthurmfgtab@gmail.com>
2019-09-26 16:06:59 -0300

    uploading week-1 project

---
+++ week-1/js/script.js
@@ -0,0 +1,27 @@
+function pesquisarTarefa() {
+    
+    var input
+    var filter
+    var table
+    var tr
+    var td
+    var txtValue
+    
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    
+    for (var i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
